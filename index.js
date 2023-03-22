091fd3dbee73511a814ea6b14e83333d4ff74593
rossivk <rosica.kirkova@gmail.com>
2023-03-22 13:51:16 +0000

    submenu for desktop

--- index.js
+++ index.js
@@ -1,51 +1,31 @@
-// Hamburger timeline
+//show sub-menu on hover over services
 
-const upper = document.getElementsByClassName("upper");
-const middle = document.getElementsByClassName("middle");
-const middle1 = document.getElementsByClassName("middle1");
-const lower = document.getElementsByClassName("lower");
-let tl2 = new TimelineLite({ paused: true, reversed: true });
+const servicesBtn = document.getElementsByClassName('services-btn')[0];
+const dropDownSubMenu = document.getElementsByClassName('dropdown-content')[0];
+const mainMenu = document.getElementById('main-menu').children[1];
+// console.log(mainMenu);
+const marker = document.getElementById('marker');
 
-tl2
-  .to(
-    upper,
-    0.5,
-    {
-      attr: { d: "M11,2 L2,11" },
-      x: 1,
-      stroke: `#191919`,
-      strokeWidth: 2,
-      ease: Power2.easeInOut
-    },
-    "start"
-  )
+servicesBtn.addEventListener('mouseover', function () {
+  dropDownSubMenu.classList.add('show-submenu');
+  mainMenu.classList.add('marker');
 
-  .to(middle, 0.5, { autoAlpha: 0, stroke: `#191919` }, "start")
-  .to(middle1, 0.5, { autoAlpha: 0, stroke: `#CB5302` }, "start")
-  .to(
-    lower,
-    0.5,
-    {
-      attr: { d: "M2,2 L11,11" },
-      x: 1,
-      stroke: `#CB5302`,
-      strokeWidth: 2,
-      ease: Power2.easeInOut
-    },
-    "start"
-  );
+});
 
-document
-  .querySelector(".hamburger-icon")
-  .addEventListener("click", function () {
-    tl2.reversed() ? tl2.play() : tl2.reverse();
-    document.getElementById("container").style.transitionDuration = '0.5s';
+servicesBtn.addEventListener('mouseout', function () {
+  if (!mouseOverOut(dropDownSubMenu)) {
+    dropDownSubMenu.classList.remove('show-submenu');
+    mainMenu.classList.add('marker');
+  }
+});
 
-    if (document.getElementById("container").style.opacity === '1') {
-      document.getElementById("container").style.opacity = '0';
-    } else {
-      document.getElementById("container").style.opacity = '1';
-      document.getElementById("container").style.pointerEvents = 'auto';
-
-    }
+function mouseOverOut(el) {
+  el.addEventListener('mouseover', () => {
+    el.classList.add('show-submenu');
   });
+  el.addEventListener('mouseout', () => {
+    el.classList.remove('show-submenu');
+  });
+
+}
+
